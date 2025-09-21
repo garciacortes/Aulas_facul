ec86a96aabc237e694ff92a1b76926e9f9c763e3
Lucas Garcia <lucascortes2003@hotmail.com>
2025-09-21 08:20:28 -0300

    add part2 prova1

--- Inteligencia Artificial/prova1/Controllers/interface_controller.py
+++ Inteligencia Artificial/prova1/Controllers/interface_controller.py
@@ -1,7 +1,8 @@
 class interfaceController():
-  def __init__(self, model_Table_training, model_Table_testing, view=None):
+  def __init__(self, model_Table_training, model_Table_testing, network_Operation, view=None):
     self.model_Table_training = model_Table_training
     self.model_Table_testing = model_Table_testing
+    self.network_Operation = network_Operation
     self.view = view
     
     self.connections()
@@ -9,3 +10,4 @@ class interfaceController():
   def connections(self):
     self.view.table_training.setModel(self.model_Table_training)
     self.view.table_testing.setModel(self.model_Table_testing)
+    self.view.btn_treinar.clicked.connect(self.network_Operation.btn_treinar)
--- Inteligencia Artificial/prova1/main.py
+++ Inteligencia Artificial/prova1/main.py
@@ -3,6 +3,7 @@ from PyQt6.QtWidgets import QApplication
 
 from Controllers.interface_controller import interfaceController
 from Controllers.neural_controller import NeuralNetworkController
+from Models.business.redeCalculo import Network_Operation
 from Models.colorsModel import NeuralNetworkModel
 from Models.business.positions import neural_calculate
 from Models.tableModel import TableModel
@@ -17,9 +18,10 @@ if __name__ == "__main__":
     model = NeuralNetworkModel(positions, lines, layer_indices)
     model_Table_training = TableModel()
     model_Table_testing = TableModel()
+    network_Operation = Network_Operation(model_Table_training, model_Table_testing)
     controller = NeuralNetworkController(model) 
     view = Interface(model, controller)
-    interface_controller = interfaceController(model_Table_training, model_Table_testing, view);
+    interface_controller = interfaceController(model_Table_training, model_Table_testing, network_Operation, view);
     
     controller.canvas = view.canvas
 
